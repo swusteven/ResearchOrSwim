938c1794b0888711dea540179ead5be69efe9604
swusteven <swusteven@yahoo.com>
2022-02-05 18:18:02 -0500

    set up FinHub API connection

--- src/scripts/api_data.js
+++ src/scripts/api_data.js
@@ -1,12 +1,21 @@
 
 const ApiData = console.log("this is the api_data.js file")
 
-//fetch companyName
-fetch("https://sandbox.iexapis.com/stable/stock/IBM/quote?token=Tpk_b837b391ec71490ebdc914ca52df4873 ")
+
+const apiURL = "https://finnhub.io/api/v1";
+const token = "sandbox_c7vfjqiad3i9ikp81lg0"
+
+//--- change this only
+const endPoint = "/stock/financials?symbol=AAPL&statement=bs&freq=annual"
+
+fetch(`${apiURL}${endPoint}&token=${token}`)
     .then(res => res.json())
-    .then(data => console.log(data.companyName))
+    .then(data => showData(data.financials))
 
-
-
+function showData(data){
+    data.forEach(element => {
+        console.log(element.buy);
+    });
+}
 
 export default ApiData
