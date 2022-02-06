f4d286e335051130ad4e8a5e966e421b580d8fef
swusteven <swusteven@yahoo.com>
2022-02-05 19:21:58 -0500

    added endpoints for earnings and one year historical closing price

--- src/scripts/api_data.js
+++ src/scripts/api_data.js
@@ -1,21 +1,49 @@
 
 const ApiData = console.log("this is the api_data.js file")
 
+// //Earnings -----------
+// const apiURL = "https://finnhub.io/api/v1";
+// const token = "sandbox_c7vfjqiad3i9ikp81lg0"
+// //--- change this only
+// const endPoint = "/stock/financials?symbol=AAPL&statement=ic&freq=annual"
 
+// fetch(`${apiURL}${endPoint}&token=${token}`)
+//     .then(res => res.json())
+//     .then(data => showData(data.financials))
+
+
+// function showData(data){
+//     data.forEach(element => {
+//         const el = document.querySelector(".test");
+//         const newLi = document.createElement("li");
+        
+//         newLi.innerText = `Year: ${element.year} - ${element.revenue}`;
+//         el.appendChild(newLi);
+//     });
+// }
+
+
+
+//Historical Closing -----------
 const apiURL = "https://finnhub.io/api/v1";
 const token = "sandbox_c7vfjqiad3i9ikp81lg0"
-
 //--- change this only
-const endPoint = "/stock/financials?symbol=AAPL&statement=bs&freq=annual"
+const endPoint = "/stock/candle?symbol=AAPL&resolution=D&from=1614644236&to=1643674636"
 
 fetch(`${apiURL}${endPoint}&token=${token}`)
     .then(res => res.json())
-    .then(data => showData(data.financials))
+    // .then(data => console.log(data));
+    .then(data => showData(data))
+
 
 function showData(data){
-    data.forEach(element => {
-        console.log(element.buy);
-    });
+    const el = document.querySelector(".test");
+    for (let i = 0; i < data.c.length; i++) {
+        const newLi = document.createElement("li");        
+        newLi.innerText = `Date: ${data.t[i]} - ${data.c[i]}`;    // t is date and c is closing price
+        el.appendChild(newLi);
+    }
 }
+
 
 export default ApiData
