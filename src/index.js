dbbe1cf613d9cd440351c89c70a2252ed5a70811
swusteven <swusteven@yahoo.com>
2022-02-09 00:01:29 -0500

    added autofill when page loaded

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import "./styles/index.css"
 import Util from './scripts/util';
 import apiCalls from './scripts/api_calls'
 
+//Autofill when page loaded
+document.addEventListener("DOMContentLoaded", autofill)
+
+function autofill(){
+    document.querySelector("#ticker-symbol").value = 'AAPL';
+    apiCalls()
+}
 
 //Set event listener on the retrieve button.
 const retrieveButton = document.querySelector("input[id='retrieve-button']");   
 retrieveButton.addEventListener("click", apiCalls);
-
--- src/scripts/api_calls.js
+++ src/scripts/api_calls.js
@@ -7,9 +7,9 @@ import analytics from "./draw_charts/analytics";
 
 const apiCalls  = async ()=>{
     removeExistingContents();
- 
+        
     const ticker = document.querySelector("#ticker-symbol").value;
-       
+       debugger
     //--- Finhub earning endpoint API
     const apiURL = "https://finnhub.io/api/v1";
     const token = "sandbox_c7vfjqiad3i9ikp81lg0";
